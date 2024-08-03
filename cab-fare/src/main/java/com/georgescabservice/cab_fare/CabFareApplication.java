430ee3ac6cefb6f4cd85c72a3530dcc096a1c9cd
Connor Sandry <connor9020@gmail.com>
2024-08-03 19:15:57 -0400

    project structure

---
+++ cab-fare/src/main/java/com/georgescabservice/cab_fare/CabFareApplication.java
@@ -0,0 +1,13 @@
+package com.georgescabservice.cab_fare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CabFareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CabFareApplication.class, args);
+	}
+
+}
---
+++ cab-fare/src/test/java/com/georgescabservice/cab_fare/CabFareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.georgescabservice.cab_fare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CabFareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
